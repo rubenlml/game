b7e7317ad8aa7d88cd4dd5d0c92aefb8a62ada48
rubenlml <theras197@gmail.com>
2023-03-10 10:27:15 -0600

    inicio del canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert("hola mundo");
+window.addEventListener('load',init);
+
+function init(){
+    var lienzo = document.querySelector('#videojuego');
+    var contexto = lienzo.getContext('2d');
+    contexto.fillRect('20,20,50,50');
+    ctxfilstyle = 'red';
+}
