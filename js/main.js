70083aef8c50f49fcc9e80c687018b2c3e765a8a
rubenlml <theras197@gmail.com>
2023-03-10 10:36:07 -0600

    fix

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ function init(){
     var lienzo = document.querySelector('#videojuego');
     var contexto = lienzo.getContext('2d');
     contexto.fillRect('20,20,50,50');
-    contexto.filStyle = 'red';
+    contexto.fillStyle = 'red';
 }
